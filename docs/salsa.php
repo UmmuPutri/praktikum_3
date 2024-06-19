b2cb583a22b12ed13c009f3d00ee72ffc4d17221
UmmuPutri <ummuputri627@gmail.com>
2024-06-19 21:33:28 +0700

    Add files via upload

---
+++ docs/salsa.php
@@ -0,0 +1,8 @@
+<?php
+echo"<center>Nama Lengkap: ".$_POST['nama']."</center><br>";
+echo"<center>NIM: ".$_POST['nim']."</center><br>";
+echo"<center>Tempat Lahir: ".$_POST['tempat']."</center><br>";
+echo"<center>Tanggal Lahir: ".$_POST['tanggal']."</center><br>";
+echo"<center>No.Telp: ".$_POST['tlp']."</center><br>";
+echo"<center>Email: ".$_POST['email']."</center><br>";
+?>
